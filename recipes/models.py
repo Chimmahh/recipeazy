aaeea7c60a6e5dd3b0ce5d008f60308bcf979bba
Tim Wilson <timwilsonr@gmail.com>
2023-12-19 22:04:30 -0500

    add units to new file and finish RecipeBuildItem class

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..static import units
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
@@ -10,43 +11,13 @@ class Recipe(models.Model):
     # yield_unit = 
 
 class Ingredient(models.Model):
-    UNIT_GRAM = 'g'
-    UNIT_KILOGRAM = 'kg'
-    UNIT_OUNCE_MASS = 'ozm'
-    UNIT_POUND = 'lb'
-    UNIT_LITER = 'l'
-    UNIT_TEASPOON = 'tsp'
-    UNIT_TABLESPOON = 'tbsp'
-    UNIT_OUNCE_FLUID = 'oz'
-    UNIT_CUP = 'c'
-    UNIT_QUART = 'qt'
-    UNIT_GALLON = 'gal'
-    UNIT_EACH = 'ea'
-    UNIT_PACK = 'pk'
-    UNIT_CASE = 'cs'
-    UNIT_CHOICES = [
-        (UNIT_GRAM, 'gram'),
-        (UNIT_KILOGRAM, 'kilogram'),
-        (UNIT_OUNCE_MASS, 'ounce (mass)'),
-        (UNIT_POUND, 'pound'), 
-        (UNIT_LITER, 'liter'), 
-        (UNIT_TEASPOON, 'teaspoon'), 
-        (UNIT_TABLESPOON, 'tablespoon'), 
-        (UNIT_OUNCE_FLUID, 'ounce (fluid)'), 
-        (UNIT_CUP, 'cup'), 
-        (UNIT_QUART, 'quart'), 
-        (UNIT_GALLON, 'gallon'),
-        (UNIT_EACH, 'each'),
-        (UNIT_PACK, 'pack'),
-        (UNIT_CASE, 'case')
-    ]
-    
     name = models.CharField(max_length = 255)
     created_date = models.DateTimeField(auto_now_add = True)
     last_update = models.DateTimeField(auto_now=True)
-    unit = models.Choices(max_length=4, choices=UNIT_CHOICES, default=UNIT_EACH)
+    unit = models.Choices(max_length=4, choices=units.UNIT_CHOICES, default=units.UNIT_EACH)
 
 class RecipeBuildItem(models.Model):
-    master_recipe = models.ManyToOneRel(Recipe, on_delete=models.CASCADE)
-    # build_recipe = models.OneToOneField(Recipe,)
-    # build_ingredient = models.CharField(Ingredient)
+    master_recipe = models.ManyToManyRel(Recipe, on_delete=models.CASCADE)
+    build_recipe = models.ManyToManyRel(Recipe, on_delete=models.PROTECT, null=True)
+    build_ingredient = models.ManyToManyRel(Ingredient, on_delete=models.PROTECT, null=True)
+    build_quantity = models.FloatField()
---
+++ static/units.py
@@ -0,0 +1,30 @@
+UNIT_GRAM = 'g'
+UNIT_KILOGRAM = 'kg'
+UNIT_OUNCE_MASS = 'ozm'
+UNIT_POUND = 'lb'
+UNIT_LITER = 'l'
+UNIT_TEASPOON = 'tsp'
+UNIT_TABLESPOON = 'tbsp'
+UNIT_OUNCE_FLUID = 'oz'
+UNIT_CUP = 'c'
+UNIT_QUART = 'qt'
+UNIT_GALLON = 'gal'
+UNIT_EACH = 'ea'
+UNIT_PACK = 'pk'
+UNIT_CASE = 'cs'
+UNIT_CHOICES = (
+    (UNIT_GRAM, 'gram'),
+    (UNIT_KILOGRAM, 'kilogram'),
+    (UNIT_OUNCE_MASS, 'ounce (mass)'),
+    (UNIT_POUND, 'pound'), 
+    (UNIT_LITER, 'liter'), 
+    (UNIT_TEASPOON, 'teaspoon'), 
+    (UNIT_TABLESPOON, 'tablespoon'), 
+    (UNIT_OUNCE_FLUID, 'ounce (fluid)'), 
+    (UNIT_CUP, 'cup'), 
+    (UNIT_QUART, 'quart'), 
+    (UNIT_GALLON, 'gallon'),
+    (UNIT_EACH, 'each'),
+    (UNIT_PACK, 'pack'),
+    (UNIT_CASE, 'case')
+)
