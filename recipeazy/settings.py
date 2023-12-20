95c9232e18281f288f91a96733c542cfc131d997
Tim Wilson <timwilsonr@gmail.com>
2023-12-20 18:43:27 -0500

    pre-database population

--- recipeazy/settings.py
+++ recipeazy/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'recipeazy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recipeazy',
+        'HOST': '127.0.0.1',
+        'PASSWORD': 'Tr1mm@ny'
     }
 }
 
--- recipes/models.py
+++ recipes/models.py
@@ -4,7 +4,7 @@ from static import units
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    created_date = models.DateTimeField(auto_now_add = True)
+    created_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
     # price = models.DecimalField(max_digits=6, decimal_places=2)
     yield_quantity = models.IntegerField()
@@ -12,7 +12,7 @@ class Recipe(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
-    created_date = models.DateTimeField(auto_now_add = True)
+    created_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
     unit = models.CharField(max_length=4, choices=units.UNIT_CHOICES, default=units.UNIT_EACH)
 
