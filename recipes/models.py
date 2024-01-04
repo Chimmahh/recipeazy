6bb6f164292911bdbf40da0bcf9ecee033384b07
Tim Wilson <timwilsonr@gmail.com>
2024-01-03 21:01:21 -0500

    Start admin site

--- recipes/models.py
+++ recipes/models.py
@@ -4,16 +4,16 @@ from static import units
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    last_update = models.DateTimeField(auto_now=True, null=True)
     # price = models.DecimalField(max_digits=6, decimal_places=2)
     yield_quantity = models.IntegerField()
     # yield_unit = 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
-    created_date = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    last_update = models.DateTimeField(auto_now=True, null=True)
     unit = models.CharField(max_length=4, choices=units.UNIT_CHOICES, default=units.UNIT_EACH)
 
 class RecipeBuildItem(models.Model):
